209585b0a32242e266677f99b3dee06199ef2caf
Paweł Mucha <pawel@mucha.com>
2020-08-17 11:12:25 +0200

    Added definition of 1st custom component wrapper: <pm-v-btn />

---
+++ components/PmVBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn
+    :ripple="(this.$attrs.ripple) ? this.$attrs.ripple : false"
+    :elevation="(this.$attrs.elevation) ? this.$attrs.elevation : 0"
+    v-bind="$attrs"
+  >
+    <slot></slot>
+  </v-btn>
+</template>
+
+
+<script>
+export default {
+  inheritAttrs: false,
+  data () {
+    return { }
+  },
+}
+</script>
