c271dc0c3b183a0a1a273c94b57c3b8476f10f44
Pawel <pawel@mucha.com>
2020-09-08 14:58:43 +0200

    Complete redesign of default/home page.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,118 +1,290 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
+
+    <!-- #region TOP BAR -->
+    <v-app-bar
+      color="background"
+      class="px-0"
+      elevation="0"
       fixed
-      app
+      height="56"
+      :style="{
+        'border-bottom': `0.33px solid ${ ($vuetify.theme.dark) ? 'var(--v-secondary-darken2)' : 'var(--v-secondary-lighten5)' } !important`,
+        'z-index': '100',
+        'background':
+          ($vuetify.theme.dark) ?
+            'linear-gradient(60deg, var(--v-background-base) 30%, var(--v-background-lighten5) 350%)'
+            :
+            'linear-gradient(175deg, var(--v-background-base) 30%, var(--v-background-darken4) 505%) !important'
+      }"
     >
-      <v-list>
+      <v-icon class="mr-3">mdi-vuejs</v-icon>
+      <v-toolbar-title>Dashboard</v-toolbar-title>
+      <v-spacer />
+      <v-btn :ripple="false" icon @click="snackbar = true">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+    <!-- #endregion TOP BAR -->
+
+    <v-snackbar
+      v-model="snackbar"
+      top
+      info
+      color="info"
+      elevation="0"
+      class="pt-14"
+      rounded
+    >
+      There is nothing to search for now.
+    </v-snackbar>
+
+    <!-- #region MAIN NAVIGATION -->
+    <v-navigation-drawer
+      v-model="isNaviVisible" app
+      :right="isMobile"
+      :temporary="isTemporary"
+      touchless
+      mobile-breakpoint="400"
+      :style="{
+        'padding-top': '56px',
+        'padding-bottom': '50px',
+        'width': `${sidebarWidth}px`,
+        'max-height': 'calc(100% - 50px)',
+        'background': 'var(--v-background-lighten1)',
+        'background': 'linear-gradient(45deg, var(--v-background-base) -30%, var(--v-background-lighten2) 100%)',
+        'justify-content': 'flex-start'
+      }"
+    >
+      <v-img src="pm-circle.png" width="60%" class="mx-auto mt-7" />
+      <v-list :style="
+        // If the drawer is on right:
+        (isMobile) ?
+          // it should be on bottom
+          `position: absolute; bottom: 0; width: 100%`
+          :
+          `margin-top: 1rem`
+      "
+      >
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
+          :ripple="false"
           :to="item.to"
+          :disabled="item.disabled"
           router
           exact
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+          <v-list-item-action class="mr-3">
+            <v-icon small color="grey">{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title class="list-item" v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
+    <!-- #endregion MAIN NAVIGATION -->
+
+
+    <!-- #region MAIN WEBSITE CONTENT -->
+    <v-main :style="{
+      'padding-left':
+        (isMobile) ? // It it's mobile (so the navbar is right)
+          0 // there should be no left padding
+          :
+          ( // otherwise
+            (isNaviVisible) ? // if navbar is visible
+              `${sidebarWidth}px` // the padding should be ...px
+              :
+              0 // if no navbar then padding should be 0
+          ),
+      'padding-right':
+        (isMobile) ? ( // If it's mobile (so the navbar is right)
+          (isTemporary) ? // If it's also overlaid
+            0 // there should be no right padding
+            :
+            (isNaviVisible) ? // not overlaid but visible
+              `${sidebarWidth}px` // padding length
+              :
+              0 // no padding
+        )
+          :
+          0, // if no navbar on the right, right padding should be 0
+    }"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
       <v-container>
-        <nuxt />
+        <v-slide-y-reverse-transition hide-on-leave>
+          <nuxt />
+        </v-slide-y-reverse-transition>
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <!-- #endregion MAIN WEBSITE CONTENT -->
+
+
+    <!-- #region FOOTER -->
     <v-footer
-      :absolute="!fixed"
       app
+      color="background"
+      class="pa-0"
+      :style="{
+        'border-top': `0.33px solid ${ ($vuetify.theme.dark) ? 'var(--v-secondary-darken2)' : 'var(--v-secondary-lighten5)' } !important`,
+        'z-index': 100,
+      }"
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <!-- #region BOTTOM NAVIGATION -->
+      <v-bottom-navigation
+        :value="activeBtn"
+        background-color="transparent"
+        :class="`
+          ${ (isMobile) ? 'justify-end' : 'justify-center' }
+          elevation-0
+        `"
+      >
+
+        <v-btn @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+          <span>
+            {{
+              $vuetify.theme.dark ? "Go light" : "Go dark"
+            }}
+          </span>
+          <v-icon>mdi-brightness-6</v-icon>
+        </v-btn>
+
+        <v-btn href="https://pawelmucha.com/upwork" target="_blank">
+          <span>Upwork</span>
+          <v-icon>mdi-desktop-classic</v-icon>
+        </v-btn>
+
+        <v-btn href="mailto:pawel@mucha.com">
+          <span>E-mail</span>
+          <v-icon>mdi-email</v-icon>
+        </v-btn>
+
+        <pm-v-btn
+          hover="0" class="ma-0 pa-0 d-xl-none"
+          :style="{
+            'display': (isMobile) ? 'flex' : 'none',
+            'min-width': ($vuetify.breakpoint.width <= 320) ? '32px' : '64px'
+          }"
+          @click.stop="isNaviVisible = !isNaviVisible"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </pm-v-btn>
+      </v-bottom-navigation>
+      <!-- #endregion BOTTOM NAVIGATION -->
     </v-footer>
+    <!-- #endregion FOOTER -->
   </v-app>
 </template>
 
+/****************************************/
 
 <script>
+import PmVBtn from "../components/PmVBtn"
+
 export default {
+  components: { PmVBtn },
+
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
       items: [
         {
-          icon: "mdi-apps",
-          title: "Welcome",
+          icon: "mdi-view-dashboard",
+          title: "Dashboard",
           to: "/",
         },
         {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire",
+          icon: "mdi-lead-pencil",
+          title: "Articles",
+          to: "/articles/customizing-css",
+        },
+        {
+          icon: "mdi-react",
+          title: "React",
+          to: "#",
+          disabled: true,
+        },
+        {
+          icon: "mdi-angular",
+          title: "Angular",
+          to: "#",
+          disabled: true,
+        },
+        {
+          icon: "mdi-laravel",
+          title: "Laravel",
+          to: "#",
+          disabled: true,
         },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Vuetify.js",
+      title: "Paweł Mucha Vuetify admin template example",
+      activeBtn: 1,
+      isNaviVisible: false,
+      sidebarWidth: 192,
+      snackbar: false,
+    }
+  },
+
+  // DRY values
+  computed: {
+    breadcrumbs() { return this.$store.state.breadcrumbs },
+    isMobile() { return this.$vuetify.breakpoint.mobile },
+    isTemporary() { return this.$vuetify.breakpoint.mobile && !this.$vuetify.breakpoint.smAndUp },
+  },
+
+  watch: {
+    // This surprisingly works very well
+    "$vuetify.breakpoint.mobile"() {
+      this.isNaviVisible = true
+    },
+  },
+
+  mounted() {
+    if (!this.isMobile) {
+      this.isNaviVisible = true
     }
   },
 }
 </script>
+
+/****************************************/
+
+<style lang="scss">
+#app {
+  background-color: var(--v-background-base);
+}
+
+.container {
+  padding: ($spacer * 3) ($spacer * 3) 0 ($spacer * 3);
+}
+
+.topSearchColumn {
+  @media #{map-get($display-breakpoints, "xs-only")} {
+    &:focus-within {
+      .v-input {
+        position: absolute;
+        right: 0; top: 10px;
+        width: 90%;
+      }
+    }
+  }
+} // .topSearchColumn
+
+.list-item {
+  text-transform: uppercase;
+  font-size: 0.75em;
+}
+
+.v-input {
+  // Makes the magnifying glass icon in the top bar
+  // a bit closer to the left.
+  .v-icon.mdi-magnify {
+    margin-left: -12px;
+    margin-bottom: -2px;
+
+    opacity: 0.6;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,213 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+  <v-layout row style="padding-top: 56px">
+
+    <v-flex
+      xs12
+      class="pl-1 pt-1"
+    >
+
+      <v-card
+        color="transparent"
+        flat
+      >
+
+        <v-card-title class="headline">Welcome!</v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >discord</a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >issue board</a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em>
-              <small>&mdash; John Leider</small>
-            </em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer">Nuxt Documentation</a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Nuxt GitHub</a>
+          Welcome to this simple dashboard demo made with <em>Vue.js</em>, <em>Nuxt.js</em>,
+          and <em>Vuetify.js</em>. It showcases also other libraries not related to Vue,
+          such as <em>Chart.js</em>.
+          It is still work in progress and will be constantly
+          filled with more content.
+          To switch to the light theme press the button
+          at the bottom. There is also a link to my Upwork profile.
+          The whole source code is publicly available on
+          <a href="https://github.com/pmucha" target="_blank">Github</a>.
+        </v-card-text>
+      </v-card>
+
+    </v-flex>
+
+    <v-card class="d-block col-12 elevation-0" color="transparent">
+      <div style="width: 100%; margin: 64px auto 32px">
+        <pm-chart-js />
+      </div>
+    </v-card>
+
+    <v-flex xs12 md6 class="pl-1 pt-1">
+
+      <v-card
+        color="background lighten-1"
+        style="height: 100%"
+        flat
+      >
+        <v-card-title
+          :class="`
+            headline
+            ${ ($vuetify.theme.dark) ? 'font-weight-thin' : 'font-weight-light' }
+            font-size-larger
+          `"
+        >Customizing CSS</v-card-title>
+        <v-card-text>
+          <em>Vuetify.js</em> and <em>Nuxt.js</em>
+          allow for customizing their CSS and SCSS
+          variables. This short article describes
+          how to do this.
+        </v-card-text>
+        <v-card-text>
+          <v-chip
+            class="ma-2"
+            color="primary"
+          >
+            Tutorial
+          </v-chip>
+          <v-chip
+            class="ma-2"
+            color="secondary"
+          >
+            SCSS
+          </v-chip>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
+          <pm-v-btn
+            class="info darken-1 px-4"
+            nuxt to="articles/customizing-css"
+          >
+            Read the article
+          </pm-v-btn>
         </v-card-actions>
       </v-card>
+
+    </v-flex>
+    <v-flex xs12 md6 class="pl-1 pt-1">
+
+      <v-card
+        color="background lighten-1"
+        style="height: 100%"
+        flat
+      >
+        <v-card-title
+          :class="`
+            headline
+            ${ ($vuetify.theme.dark) ? 'font-weight-thin' : 'font-weight-light' }
+            font-size-larger
+          `"
+        >Extending components</v-card-title>
+        <v-card-text>
+          <em>Vue.js</em> makes it ease to extend
+          its components. This short article
+          shows how to do this, using a <em>Vuetify.js</em>
+          component as a base.
+        </v-card-text>
+        <v-card-text>
+          <v-chip
+            class="ma-2"
+            color="primary"
+          >
+            Tutorial
+          </v-chip>
+          <v-chip
+            class="ma-2"
+            color="info"
+          >
+            Vuetify
+          </v-chip>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <pm-v-btn
+            class="info darken-1 px-4"
+            nuxt to="articles/extending-components"
+          >
+            Read the article
+          </pm-v-btn>
+        </v-card-actions>
+      </v-card>
+
     </v-flex>
   </v-layout>
 </template>
 
 
+<style>
+.font-size-larger {
+  font-size: 1.25em;
+}
+</style>
+
+
 <script>
-import Logo from "~/components/Logo.vue"
-import VuetifyLogo from "~/components/VuetifyLogo.vue"
+import PmChartJs from "../components/PmChartJs"
+import PmVBtn from "../components/PmVBtn"
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  components: { PmVBtn, PmChartJs },
+
+  data () {
+    return {
+      // dark: true,
+      email: "",
+      agreement: false,
+      dialog: false,
+      rules: {
+        agreementRequired: (v) => { return !!v || "You must agree to continue" },
+        emailRequired: (v) => { return (v && !!v.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) || "You must provide a valid e-mail address" },
+        emailCounter: (v) => { return !!v || "You must provide an e-mail address" },
+      },
+    }
+  },
+
+  computed: {
+    formValid: {
+      get() {
+        console.log("Get formValid", this.agreementRequired, this.emailRequired, this.emailCounter)
+        return this.agreementRequired && this.emailRequired && this.emailCounter
+      },
+      set() { },
+    },
+  },
+
+  mounted() {
+    this.$vuetify.goTo(0, { duration: 0 })
+
+    // this.dark = this.$vuetify.theme.dark
+
+    this.$store.commit("setBreadcrumbs", [
+      {
+        text: "Home",
+        disabled: false,
+        to: "/",
+      },
+    ])
+
+    this.$nextTick(() => {
+      this.$el.querySelectorAll("[data-transition]").forEach((v) => {
+        // console.log(v.__vue__)
+        v.__vue__.appear = true
+      })
+      // for (const i in this.$el.querySelectorAll("[data-transition]")) {
+      //   console.log(i)
+      //   // i.style.display = "initial"
+      // }
+    })
+  },
+
+  methods: {
+    submitForm () {
+      this.$refs.form.validate()
+    },
+    validateAndSubmit() {
+      const form = this.$refs.form
+
+      console.log("Validate and submit", form)
+
+      if (form.validate()) {
+        console.log("Validated successfully")
+        form.$el.submit()
+      } else {
+        console.log("Validation failed")
+      }
+    },
   },
 }
 </script>
